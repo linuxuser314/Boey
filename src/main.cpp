2808236dec39102e61500bee99d999be1ae3c056
avr_dude <creed_truman77@my.albemarle.edu>
2026-04-23 15:34:14 -0400

    failing

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ int main(void){
 
   while(true){
     
-    if(checkForButtonPress(BUTTON)){
+      if(checkForButtonPress(BUTTON)){
       buttonIsPressed = !buttonIsPressed;
       if(buttonIsPressed){
         serialTransmitMsg('B', 1);
@@ -59,5 +59,6 @@ int main(void){
     }
     myDelay(100);
   }
-	
+    
+  
 }
